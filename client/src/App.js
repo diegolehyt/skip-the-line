49cf3da7be5b5c4e2abada13b8277dec4ce2c6ac
Dustin Irving <dustin.irving@gmail.com>
2020-08-07 19:14:17 -0400

    Set up google oauth

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
 // import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Wrapper from './components/Wrapper'
 
 const styles = {
   headerB: {
-    fontFamily: "'Heebo', sans-serif" 
+    fontFamily: "'Heebo', sans-serif"
   },
   headerC: {
-    fontFamily: "'Trade Winds', cursive" 
+    fontFamily: "'Trade Winds', cursive"
   }
 }
 
-function App() {
+function App () {
   return (
     <Router>
       <header style={styles.headerB}>
-        <Navbar />
-        <Wrapper basename="/">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
+        <Wrapper>
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/login' component={Login} />
         </Wrapper>
         {/* <Footer /> */}
       </header>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
