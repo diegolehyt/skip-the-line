7061dd21cdb71ad1b2cb1551a59430f786375dc5
Dustin Irving <dustin.irving@gmail.com>
2020-08-14 16:41:05 -0400

    Added socket io

--- client/src/components/HomeContent/index.js
+++ client/src/components/HomeContent/index.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from "react";
-import StoreBlock from "../StoreBlock";
+import React, { useState, useEffect } from 'react'
+import StoreBlock from '../StoreBlock'
 import axios from 'axios'
-import "./style.css";
-import storesList from "./stores.json"
+import './style.css'
+import storesList from './stores.json'
 import Cookies from 'js-cookie'
-import userAPI from "./userAPI.json"
-import {connect } from 'react-redux'
-import {getUser} from '../../actions/userActions'
+import userAPI from './userAPI.json'
+import { connect } from 'react-redux'
+import { getUser } from '../../actions/userActions'
+import io from 'socket.io-client'
+const socket = io('http://localhost:3001')
+socket.on('event', event => {
+  console.log(event.message)
+})
 
 const styles = {
   headerB: {
-    color: "#04375a" 
+    color: '#04375a'
   },
   headerC: {
     fontFamily: "'Trade Winds', cursive",
-    color: '#04375a' 
+    color: '#04375a'
   },
   headerD: {
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: '100px',
     marginBottom: '40px'
   },
   headerE: {
-    width: "300px",
+    width: '300px',
     height: '300px'
   },
   cities: {
@@ -30,15 +35,14 @@ const styles = {
   }
 }
 
-function HomeContent({getUser, users}) {
-
-  const [stores, setStores] = useState([]);
-  const [line, setLine] = useState([]);
-  const [store, setStore] = useState({});
+function HomeContent ({ getUser, users }) {
+  const [stores, setStores] = useState([])
+  const [line, setLine] = useState([])
+  const [store, setStore] = useState({})
   // const [user, setUser] = useState({});
 
-  const [userOnline, setOnlineUser] = useState({});
-  const [storeAct, setStoreAct] = useState(false);
+  const [userOnline, setOnlineUser] = useState({})
+  const [storeAct, setStoreAct] = useState(false)
 
   const getStores = () => {
     // fetch("/api/logos")
@@ -52,33 +56,28 @@ function HomeContent({getUser, users}) {
 
     // setUser(userAPI)
     setStores(storesList)
-  };
+  }
 
-  
-  const getStore = (storeData) => {
-
+  const getStore = storeData => {
     console.log(storeData)
     setStore(storeData)
     setLine(storeData.inLine)
     setStoreAct(true)
-  };
+  }
 
-   
   const handleLineSubmit = () => {
-
     console.log(users.user)
     setLine(line.concat(users.user))
-  };
+    socket.emit('event', { message: 'Hello from another browser' })
+  }
 
   const handleLineCancel = () => {
-    setLine(line.filter(
-      (playerZ) => playerZ.email !== users.user.email
-    ))
-  };
+    setLine(line.filter(playerZ => playerZ.email !== users.user.email))
+  }
 
   useEffect(() => {
-    getStores();
-  }, []);
+    getStores()
+  }, [])
 
   // useEffect(() => {
   //   axios.get('/api/auth/user').then(res => {
@@ -88,73 +87,68 @@ function HomeContent({getUser, users}) {
   //   })
   // }, [])
 
-
   return (
-    <div className="container">
-      
-
-      <div className="scrollbar scrollbar-primary row flex-row flex-nowrap mt-4 pb-4" style={styles.cities}>
-        {
-          storesList.map(store => (
-            <StoreBlock store={store} onStore={getStore}/>
-          ))
-        }
-
+    <div className='container'>
+      <div
+        className='scrollbar scrollbar-primary row flex-row flex-nowrap mt-4 pb-4'
+        style={styles.cities}
+      >
+        {storesList.map(store => (
+          <StoreBlock store={store} onStore={getStore} />
+        ))}
       </div>
 
-      <div className="row d-flex justify-content-center flex-row flex-nowrap mt-4 pb-4" style={styles.cities}>
-
-        <div className="col-12">
-          {
-            setStoreAct
-            ?
+      <div
+        className='row d-flex justify-content-center flex-row flex-nowrap mt-4 pb-4'
+        style={styles.cities}
+      >
+        <div className='col-12'>
+          {setStoreAct ? (
             <>
               <p>{store.name}</p>
               <div>
-                <button onClick={handleLineSubmit} href="#!" className="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">
-                  <i class="fas fa-search text-white" aria-hidden="true">Yes</i>
+                <button
+                  onClick={handleLineSubmit}
+                  href='#!'
+                  className='btn btn-outline-white btn-md my-0 ml-sm-2'
+                  type='submit'
+                >
+                  <i class='fas fa-search text-white' aria-hidden='true'>
+                    Yes
+                  </i>
                 </button>
-                <button onClick={handleLineCancel} href="#!" className="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">
-                  <i class="fas fa-search text-white" aria-hidden="true">No</i>
+                <button
+                  onClick={handleLineCancel}
+                  href='#!'
+                  className='btn btn-outline-white btn-md my-0 ml-sm-2'
+                  type='submit'
+                >
+                  <i class='fas fa-search text-white' aria-hidden='true'>
+                    No
+                  </i>
                 </button>
               </div>
               <ul>
-                {
-                  line.map(line => (
-                    <p>{line.email}</p>
-                  ))
-                }
+                {line.map(line => (
+                  <p>{line.email}</p>
+                ))}
               </ul>
             </>
-            :
-            null
-          }
-          
+          ) : null}
         </div>
-        
       </div>
 
-      <div className="row text-black" style={{ marginTop: "20%" }}>
-        <div className="col-md-12 col-lg-12">
-          
-          <div className="row">
-
-           
-          </div>
-        
+      <div className='row text-black' style={{ marginTop: '20%' }}>
+        <div className='col-md-12 col-lg-12'>
+          <div className='row'></div>
         </div>
-        <div className="col-md-12 col-lg-12" style={{ marginTop: "5%" }}>
-        
-          <div className="row">
-
-          </div>
-          <hr className="hr-light my-4 w-75 style-two" />
+        <div className='col-md-12 col-lg-12' style={{ marginTop: '5%' }}>
+          <div className='row'></div>
+          <hr className='hr-light my-4 w-75 style-two' />
         </div>
-      
       </div>
     </div>
   )
-
 }
 
 const mapStateToProps = state => {
